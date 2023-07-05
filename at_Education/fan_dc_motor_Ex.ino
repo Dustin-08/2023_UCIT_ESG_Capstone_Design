b3001fa5130bd938cfb832bb65250c3fea1f5672
Dustin-08 <cjhjik1@gmail.com>
2023-07-05 18:08:35 +0900

    팬모터 관련 주석 추가
    팬모터 관련 주석 추가

--- at_Education/fan_dc_motor_Ex.ino
+++ at_Education/fan_dc_motor_Ex.ino
@@ -1,13 +1,22 @@
-int A = 8;
-int B = 9;
+/*
+ * L9110의 방향 관련:
+                                INA    | INB   | 결과
+                                HIGH   | LOW   | 반시계 방향 회전
+                                LOW    | HIGH  | 시계 방향 회전
+                                HIGH   | HIGH  | 회전 X
+                                LOW    | LOW   | 회전 X 
+*/
+
+int A = 8; // D8번 핀을 INA 핀으로 설정
+int B = 9; // D9번 핀을 INB 핀으로 설정
  
 void setup() {
-  pinMode(A,OUTPUT);
-  pinMode(B,OUTPUT);  
+  pinMode(A,OUTPUT); // A 핀을 출력으로 변경
+  pinMode(B,OUTPUT);  // B 핀을 출력으로 변경 
 }
  
 void loop() {
-  digitalWrite(A,LOW); //팬모터가 A방향으로 회전
+  digitalWrite(A,LOW); //팬모터가 A방향으로 회전 (시계 방향 회전)
   digitalWrite(B,HIGH);
   delay(2000); //2초 후
  
@@ -16,7 +25,7 @@ void loop() {
   delay(5000); //5초 후
   
   digitalWrite(A,HIGH);
-  digitalWrite(B,LOW); //팬모터가 B방향으로 회전
+  digitalWrite(B,LOW); //팬모터가 B방향으로 회전 (반시계 방향 회전)
   delay(2000); //2초 후
  
   digitalWrite(A,HIGH); // 전체 정지
